6925ec6e5086bbf94311f01add4cbd5f366d7a3e
sefo <fournierse@gmail.com>
2017-02-19 12:12:20 +0100

    let -> var

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,11 @@
-let express = require('express');
-let router = express.Router();
-let jwt = require('jsonwebtoken');
-let expressJwt = require('express-jwt');
-let bcrypt = require('bcrypt-nodejs');
-let models = require('../models');
+var express = require('express');
+var router = express.Router();
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
+var bcrypt = require('bcrypt-nodejs');
+var models = require('../models');
 
-let secret = 'DrKs8P5lXP16Dqn8jTwiRYBtJF25gGjF3kUeP4F08LeklnwCQGoDMouLZcija8Wpk7X1fRvtxytRwaWaRZaMEBQdrDSjRp9OGnVrfrZqosHE0Pfa6omupI5Y';
+var secret = 'DrKs8P5lXP16Dqn8jTwiRYBtJF25gGjF3kUeP4F08LeklnwCQGoDMouLZcija8Wpk7X1fRvtxytRwaWaRZaMEBQdrDSjRp9OGnVrfrZqosHE0Pfa6omupI5Y';
 
 router.post('/access', function (request, response) {
   var token = {};
